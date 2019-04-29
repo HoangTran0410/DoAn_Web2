8e74cda5d8f85418caf68db4c9cc6e35f15de763
Hoang Tran <th01634044850@gmail.com>
2019-04-29 18:19:30 +0700

    loại bỏ các link tới trang con

--- FrontEnd/echoHTML.php
+++ FrontEnd/echoHTML.php
@@ -15,11 +15,11 @@ function addTopNav()
 
             <ul class="top-nav-quicklink flexContain">
                 <li><a href="index.php"><i class="fa fa-home"></i> Trang chủ</a></li>
-                <li><a href="tintuc.html"><i class="fa fa-newspaper-o"></i> Tin tức</a></li>
-                <li><a href="tuyendung.html"><i class="fa fa-handshake-o"></i> Tuyển dụng</a></li>
-                <li><a href="gioithieu.html"><i class="fa fa-info-circle"></i> Giới thiệu</a></li>
-                <li><a href="trungtambaohanh.html"><i class="fa fa-wrench"></i> Bảo hành</a></li>
-                <li><a href="lienhe.html"><i class="fa fa-phone"></i> Liên hệ</a></li>
+                <li><a href=""><i class="fa fa-newspaper-o"></i> Tin tức</a></li>
+                <li><a href=""><i class="fa fa-handshake-o"></i> Tuyển dụng</a></li>
+                <li><a href=""><i class="fa fa-info-circle"></i> Giới thiệu</a></li>
+                <li><a href=""><i class="fa fa-wrench"></i> Bảo hành</a></li>
+                <li><a href=""><i class="fa fa-phone"></i> Liên hệ</a></li>
             </ul> <!-- End Quick link -->
         </section><!-- End Section -->
     </div><!-- End Top Nav  -->';
